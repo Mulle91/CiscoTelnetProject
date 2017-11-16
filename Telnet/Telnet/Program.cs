92cacc2db8a34f0d4755354f79c7cc654717ebcf
Nicklas Pihl Mathiesen <nick5666@edu.mercantec.dk>
2017-11-16 09:07:11 +0100

    ændrede i koden

--- Telnet/Telnet/Program.cs
+++ Telnet/Telnet/Program.cs
@@ -8,7 +8,7 @@ namespace Telnet
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void R1Config()
         {
             TelnetConnection T1;
             List<String> Result = new List<String>();
@@ -34,7 +34,54 @@ namespace Telnet
             });
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+        }
+        public static void Sw1Config()
+        {
+            TelnetConnection T2;
+            List<String> Result = new List<String>();
+            int lineCounter = 1;
 
+            T2 = new TelnetConnection("192.168.10.16", 23);
+            //insert code here
+            Result.ForEach(delegate (String line)
+            {
+                Console.WriteLine("{0}: {1}", lineCounter, line);
+                lineCounter++;
+            });
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+        public static int r1 = 1;
+        public static int sw1 = 2;
+        public static int both = 3;
+        private static int input;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What device do you want to config");
+            Console.WriteLine("1: R1 192.168.10.16 2: SW1 192.168.10.24 3: Both");
+            for (int i = 1; i < 4; i++) {
+                try
+                {
+                    int input = int.Parse(Console.ReadLine());
+                } catch
+                {
+                    Console.WriteLine("that's not an option");
+                }
+                if ( input == r1)
+                {
+                    R1Config();
+                } else if ( input == sw1)
+                {
+                    Sw1Config();
+                } else if ( input == both)
+                {
+                    R1Config();
+                    Console.WriteLine("så går vi videre til SW1");
+                    Console.WriteLine("press anny key too continue");
+                    Console.ReadKey();
+                    Sw1Config();
+                }
+            }
         }
     }
 }
