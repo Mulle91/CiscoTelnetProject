6e3f614bf81d0003005602c6b72192e353573c2b
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2017-11-16 08:25:20 +0100

    Telnet kode

---
+++ Telnet/Telnet/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelnetConnection T1;
+            List<String> Result = new List<String>();
+            int lineCounter = 1;
+
+            T1 = new TelnetConnection("192.168.10.16", 23);
+
+            T1.CiscoLogin("cisco");
+            T1.CiscoEnable("cisco");
+            T1.CiscoCommand("conf t");
+            T1.CiscoCommand("inter fa0/1");
+            T1.CiscoCommand("ip add 192.168.11.17 255.255.255.0");
+            T1.CiscoCommand("no shut");
+            T1.CiscoCommand("exit");
+            T1.CiscoCommand("exit");
+            T1.CiscoCommand("write");
+            Result = T1.CiscoCommand("Show start");
+
+            Result.ForEach(delegate (String line)
+            {
+                Console.WriteLine("{0}: {1}", lineCounter, line);
+                lineCounter++;
+            });
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
